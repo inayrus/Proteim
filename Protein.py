5bfdf6b51c4d445fa87f74b834643438aae689b6
Valerie <vsawirja3@gmail.com>
2019-04-06 11:13:03 +0200

    added comments and another argv check

--- Protein.py
+++ Protein.py
@@ -1,4 +1,5 @@
 import sys
+import pathlib
 from Amino import Amino
 
 class Protein(object):
@@ -7,7 +8,7 @@ class Protein(object):
     def __init__(self, file):
         """initializes a protein"""
         self.stability= 0
-        self.amino_acids = self.load_protein("ProteinData/protein_{}.txt".format(file))
+        self.amino_acids = self.load_protein("ProteinData/{}.txt".format(file))
         self.bonds = []
 
     def load_protein(self, file):
@@ -18,27 +19,39 @@ class Protein(object):
         # read in the file
         with open(file, "r") as f:
             amino_acids = []
+
             for line in f:
-                # erase the \n
+                # get rid of whitespace
                 seq = line.strip()
+
                 for index, letter in enumerate(seq):
-                    # turn each letter into an Amino
+                    # turn each letter into an Amino object
                     new_amino = Amino(index, letter)
 
+                    # add the new and prev amino to each other's connections
                     if index > 0:
                         prev_amino = amino_acids[index - 1]
-                        # add previous amino to connections
                         new_amino.set_connections(prev_amino)
-                        # add current amino to the prev amino's connections
                         prev_amino.set_connections(new_amino)
-                        print(prev_amino.conn)
+                        # print(prev_amino.conn)
 
+                    # add the new amino to the list
                     amino_acids.append(new_amino)
         return amino_acids
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("add one protein to the command line (ex. a1 or c4)")
 
+    # ensure that a filename is added to the commandline
+    if len(sys.argv) != 2:
+        print("give one protein filename to the command line (ex. protein_a1)")
+        exit(1)
+
+    # ensure that the file exists in ProteinData
+    file = pathlib.Path("ProteinData/{}.txt".format(sys.argv[1]))
+    if not file.exists():
+        print("please choose a filename that exist in the ProteinData folder")
+        exit(1)
+
+    # if all is good, create a protein object
     Protein(sys.argv[1])
