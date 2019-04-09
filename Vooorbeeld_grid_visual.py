3e964fd65018d6886845a537f96fb79ce6e5dded
ambergr <a.mayenburg@hotmail.com>
2019-04-09 14:33:47 +0200

    Voorbeeld van visualisatie??

---
+++ Vooorbeeld_grid_visual.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk()
+
+label_1 = Label(root, text= Protein.amino_acids)
+label_2 = Label(root, text=Protein.amino_acids)
+
+entry_1.grid(row = Protein.x_coordinate, column = Protein.y_coordinate)
+entry_2.grid(row = Protein.x_coordinate, column = Protein.y_coordinate)
