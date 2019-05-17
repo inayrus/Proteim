031a9d98a6b479e2f0b9c0f6b87a587ec22d27b8
Machiel Cligge <machielc@hotmail.com>
2019-05-17 13:16:46 +0200

    visualize, greedy en ribosome 3d

--- Algorithms/ribosome_fold.py
+++ Algorithms/ribosome_fold.py
@@ -20,16 +20,20 @@ def ribosome_loop(protein_filename):
 
     # fold the protein in a loop
     while True:
-        # fold a protein
-        protein, is_completed = ribosome_fold(protein_filename)
+        if sys.argv[2] == "2d":
+            # fold a 2d protein
+            protein, is_completed = ribosome_fold2d(protein_filename)
+
+        if sys.argv[2] == "3d":
+            # fold a 3dprotein
+            protein, is_completed = ribosome_fold3d(protein_filename)
 
         # only save completed proteins (ones without dead endings)
         if is_completed:
             # update the best protein list and save the best protein in a csv
             best_proteins = save_best_protein(best_proteins, protein)
 
-            
-def ribosome_fold(protein_filename):
+def ribosome_fold2d(protein_filename):
     """
     A function that folds the protein by placing its amino acids
     one by one on a grid, randomly.
@@ -48,6 +52,46 @@ def ribosome_fold(protein_filename):
             protein.place_amino([0, 0], index)
         elif index == 1:
             protein.place_amino([0, 1], index)
+
+        # for the other aminos:
+        else:
+            # get the possible places for the next amino
+            prev_amino = amino_acids[index - 1]
+            all_places = protein.get_place_options(prev_amino)
+
+            # stop if last amino is dead ending
+            if all_places == []:
+                print("Whoops folded in on myself")
+                return protein, False
+
+            # 3) pick one location to place the amino in
+            picked_place = random.choice(all_places)
+
+            # 4) update amino location & location Protein attribute
+            protein.place_amino(picked_place, amino.get_id())
+
+    return protein, True
+
+
+def ribosome_fold3d(protein_filename):
+    """
+    A function that folds the protein by placing its amino acids
+    one by one on a grid, randomly.
+    Returns protein object, success boolean (False: protein folded in on itself)
+    """
+    # initialize a new protein object
+    protein = Protein(protein_filename)
+    amino_acids = protein.get_amino_acids()
+
+    for index, amino in enumerate(amino_acids):
+        # initializes list of all possible plces for new aminoacid
+        all_places = []
+
+        # place the first two aminos
+        if index == 0:
+            protein.place_amino([0, 0, 0], index)
+        elif index == 1:
+            protein.place_amino([0, 1, 0], index)
 
         # for the other aminos:
         else:
@@ -93,8 +137,8 @@ def save_for_mean(protein, stabilities):
 if __name__ == "__main__":
 
     # ensure that a filename is added to the commandline
-    if len(sys.argv) != 2:
-        print("give one protein filename to the command line (ex. protein_a1)")
+    if len(sys.argv) != 3:
+        print("give one protein filename to the command line (ex. protein_a1) and specify 2d or 3d")
         exit(1)
 
     # ensure that the file exists in ProteinData
